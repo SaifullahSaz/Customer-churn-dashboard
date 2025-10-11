8f49a4f75aa41d35b22e9516f2c4ec880c371923
SaifullahSaz <saifullah.sazalinsyah@gmail.com>
2025-10-11 23:23:05 +0800

    Update added supabase url display for debugging

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import plotly.express as px
 from utils import preprocess_data, load_model
 from supabase import create_client, Client
 
+# Checking 
+st.write("Supabase URL:", url)
+
+
 # --- Initialize Supabase ---
 url = st.secrets["SUPABASE_URL"]
 key = st.secrets["SUPABASE_KEY"]
